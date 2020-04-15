4c7a3dbdee89a078840befa6f0c587542dc57f1a
Tung Nguyen <tongueroo@gmail.com>
2020-04-15 19:04:50 +0000

    comment out puts

--- app/jobs/hard_job.rb
+++ app/jobs/hard_job.rb
@@ -3,10 +3,10 @@ class HardJob < ApplicationJob
   def dig
     puts "HardJob Jets.config.object_id #{Jets.config.object_id}"
 
-    puts "checking email settings"
+    # puts "checking email settings"
     # puts "HardJob ActionMailer::Base.smtp_settings #{ActionMailer::Base.smtp_settings.inspect}"
     # puts "HardJob ActionMailer::Base.delivery_method #{ActionMailer::Base.delivery_method}"
     # UserMailer.new_user.deliver
-    puts "checking email settings done"
+    # puts "checking email settings done"
   end
 end
