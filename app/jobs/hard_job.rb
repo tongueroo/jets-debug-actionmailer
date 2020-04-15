0391d7283f6e86ca1cf5816852db681822a4c848
Tung Nguyen <tongueroo@gmail.com>
2020-04-15 19:08:59 +0000

    log config.action_mamiler

--- app/jobs/hard_job.rb
+++ app/jobs/hard_job.rb
@@ -2,6 +2,7 @@ class HardJob < ApplicationJob
   rate "10 hours" # every 10 hours
   def dig
     puts "HardJob Jets.config.object_id #{Jets.config.object_id}"
+    puts "HardJob Jets.config.action_mailer.object_id #{Jets.config.action_mailer.object_id} (config.action_mailer)"
 
     # puts "checking email settings"
     # puts "HardJob ActionMailer::Base.smtp_settings #{ActionMailer::Base.smtp_settings.inspect}"
